7b040da6881ee76ab85df271ce601d21520e3134
Buyi Cheng <bc2996@columbia.edu>
2023-03-11 17:56:53 -0500

    customized apis

---
+++ buinterface/src/main/java/com/byc/buinterface/client/BuClient.java
@@ -0,0 +1,79 @@
+package com.byc.buinterface.client;
+
+import cn.hutool.core.io.resource.Resource;
+import cn.hutool.core.net.url.UrlBuilder;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.crypto.digest.DigestAlgorithm;
+import cn.hutool.crypto.digest.Digester;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.byc.buinterface.model.User;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.byc.buinterface.utils.SignUtils.getSign;
+
+
+/**
+ * 调研第三方接口的客户端
+ */
+public class BuClient {
+
+    private String accessKey;
+
+    private String secretKey;
+
+    public BuClient(String accessKey, String secretKey) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+    }
+
+    public String getNameByGet(String name) {
+        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("name", name);
+        String result = HttpUtil.get("http://localhost:8001/api/name/", paramMap);
+        System.out.println(result);
+        return result;
+    }
+
+    public String getNameByPost(String name) {
+        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("name", name);
+        String result = HttpUtil.post("http://localhost:8001/api/name/", paramMap);
+        System.out.println(result);
+        return result;
+    }
+
+    public String getUsernameByPost(User user) {
+        String json = JSONUtil.toJsonStr(user);
+        HttpResponse response = HttpRequest.post("http://localhost:8001/api/name/user")
+                .charset(StandardCharsets.UTF_8)
+                .addHeaders(getHeaders(json) )
+                .body(json)
+                .execute();
+        System.out.println(response.getStatus());
+        System.out.println(response.body());
+        return response.body();
+    }
+
+    private Map<String, String> getHeaders(String body) {
+        Map<String, String> map = new HashMap<>();
+        map.put("accessKey", accessKey);
+        // 一定不能发送给后端
+//        map.put("secretKey", secretKey);
+
+        map.put("nonce", RandomUtil.randomNumbers(4));
+        map.put("body", body);
+        map.put("timestamp", String.valueOf(System.currentTimeMillis() / 1000));
+        map.put("sign", getSign(body, secretKey));
+        return map;
+    }
+
+
+}
---
+++ buinterface/src/test/java/com/byc/buinterface/BuinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byc.buinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuinterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
